0d547c52f32d638b5af07a1303382b9aedbd6bad
hiteshbiet <hiteshkumarbiet@gmail.com>
2022-05-31 18:20:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function gotowhatsapp() {
+  var name = document.getElementById("formName").value;
+  var phone = document.getElementById("phonenumber").value;
+  var service = document.getElementById("requirement").value;
+  //   var service = document.getElementById("service").value;
+
+  var url =
+    // "https://wa.me/9664263053?text=" +
+    // https://api.whatsapp.com/send?phone=919828550720&text=Hello sir
+    "https://api.whatsapp.com/send?phone=9664263053&text=" +
+    "Name: " +
+    name +
+    "%0a" +
+    "Phone: " +
+    phone +
+    "%0a" +
+    "Service: " +
+    service;
+
+  window.open(url, "_blank").focus();
+//   alert("you sended ");
+}
